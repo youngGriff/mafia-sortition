55cbf0ee43dcfdd190ef388728f7dfbad099ee66
harrion <andriy.gaidar99@gmail.com>
2018-12-10 19:45:46 +0200

    Readme changed. Firebase config added to git

---
+++ src/firebase/privateConfig.js
@@ -0,0 +1,10 @@
+export const config = {
+    apiKey: "AIzaSyDJNmtnChCRt5Gn1qU622G-H0KY0y5bx7A",
+    authDomain: "react-firebase-app-876e3.firebaseapp.com",
+    databaseURL: "https://react-firebase-app-876e3.firebaseio.com",
+    projectId: "react-firebase-app-876e3",
+    storageBucket: "react-firebase-app-876e3.appspot.com",
+    messagingSenderId: "1030425219779"
+
+};
+export default config;
